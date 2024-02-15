3ffa62dea9ca4f078d885d315b54c43852ff6dc7
Darko105 <chrifsabri700@gmail.com>
2024-02-15 06:12:05 +0200

    first commit

---
+++ components/Live.tsx
@@ -0,0 +1,63 @@
+import React, { useCallback, useState } from 'react'
+import LiveCursors from './cursor/LiveCursors'
+import { useMyPresence, useOthers } from '@/liveblocks.config';
+import CursorChat from './cursor/CursorChat';
+import { CursorMode } from '@/types/type';
+
+const Live = () => {
+    const others = useOthers();
+    const [{cursor}, updateMyPresence] = useMyPresence() as any;
+
+    const [cursorState,setCursorState] = useState({
+        mode: CursorMode.Hidden,
+    });
+
+    
+    const handlePointerMove = useCallback((event:React.PointerEvent) => {
+        event.preventDefault();
+        const x = event.clientX - event.currentTarget.getBoundingClientRect().x;
+        const  y = event.clientY - event.currentTarget.getBoundingClientRect().y;
+
+        updateMyPresence( { cursor: {x,y}});
+
+    },[])
+
+    const handlePointerLeave =useCallback((event:React.PointerEvent) => {
+        setCursorState({mode:CursorMode.Hidden})
+
+        updateMyPresence( { cursor:null,message:null});
+
+    },[])
+
+    const handlePointerDown =useCallback((event:React.PointerEvent) => {
+    
+        const x = event.clientX - event.currentTarget.getBoundingClientRect().x;
+        const  y = event.clientY - event.currentTarget.getBoundingClientRect().y;
+        updateMyPresence( { cursor: {x,y}});
+
+    },[])
+
+  return (
+    <div 
+    onPointerMove={handlePointerMove}
+    onPointerLeave={handlePointerLeave}
+    onPointerDown={handlePointerDown}
+    className=" h-[100vh] w-full flex justify-center items-center"
+    >
+        <h1 className="text-3xl text-black ">Figma Clone</h1>
+
+        {cursor && (
+            <CursorChat
+            cursor={cursor}
+            cursorState={cursorState}
+            setCursorState={setCursorState}
+            updateMyPresence={updateMyPresence}
+
+            />
+        )}
+        <LiveCursors others={ others }/>
+    </div>
+  )
+}
+
+export default Live
